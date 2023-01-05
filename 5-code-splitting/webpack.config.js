b6d52ee66a05554e78b689ebda883e5563763d74
MrParkerZ7 <neverzero9@gmail.com>
2023-01-05 16:15:23 +0700

    [update] 5 Entry Points

--- 5-code-splitting/webpack.config.js
+++ 5-code-splitting/webpack.config.js
@@ -3,27 +3,18 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  devtool: "inline-source-map",
-  entry: "./src/index.js",
   entry: {
     index: "./src/index.js",
-    print: "./src/print.js",
-  },
-  devServer: {
-    static: "./dist",
+    another: "./src/another-module.js",
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
-    publicPath: "/",
-  },
-  optimization: {
-    runtimeChunk: "single",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Development",
+      template: "./src/index.html",
+      filename: "index.html",
     }),
   ],
 };
