c70ba1e09c2ab014be1173bb717d606e2d8a62ee
MrParkerZ7 <neverzero9@gmail.com>
2023-01-05 15:10:53 +0700

    [init] 4

---
+++ 4-development/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   template: "./src/index.html",
+    //   filename: "index.html",
+    // }),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+};
